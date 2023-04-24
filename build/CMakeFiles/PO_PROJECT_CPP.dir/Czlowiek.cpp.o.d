9ba48242464cf1185339e8e86dff71633b9f5e36
varrios <33768095+varrios@users.noreply.github.com>
2023-04-24 17:27:10 +0200

    Delete build directory

--- build/CMakeFiles/PO_PROJECT_CPP.dir/Czlowiek.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/PO_PROJECT_CPP.dir/Czlowiek.cpp.o: \
-  /Users/lucjangackowski/Documents/git/PO_PROJECT_CPP/Czlowiek.cpp \
-  /Users/lucjangackowski/Documents/git/PO_PROJECT_CPP/Czlowiek.h \
-  /Users/lucjangackowski/Documents/git/PO_PROJECT_CPP/Organizm.h \
-  /Users/lucjangackowski/Documents/git/PO_PROJECT_CPP/Swiat.h \
-  /Users/lucjangackowski/Documents/git/PO_PROJECT_CPP/Zwierze.h
