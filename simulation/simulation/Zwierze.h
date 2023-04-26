73c25ae01623a2cfa9080baccaee939224e36199
varrios <lucjan.gackowski@gmail.com>
2023-04-26 18:31:15 +0200

    refactor

--- simulation/simulation/Zwierze.h
+++ simulation/simulation/Zwierze.h
@@ -4,18 +4,15 @@ using namespace std;
 
 class Zwierze : public Organizm {
 protected:
+	void wykonajRuch(int x, int y);
+	auto losujKierunek();
+	virtual bool czyMaDobryWech() const;
+	virtual bool sprawdzGatunki(Organizm* org) = 0;
 	int zasieg;
 public:
 	Zwierze();
 	Zwierze(int x, int y, Swiat* swiat);
-
 	void akcja() override;
 	void kolizja(Organizm* organizmAtakujacy) override;
-	void wykonajRuch(int x, int y);
-	auto losujKierunek();
 
-	virtual bool czyMaDobryWech() const;
-	
-
-	virtual bool sprawdzGatunki(Organizm* org) = 0;
 };
